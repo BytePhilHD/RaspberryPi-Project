9c146c8f501e3592314c97c399c3928eb9fc9000
BytePhil <phitho2018@gmail.com>
2021-12-29 10:13:33 +0100

    Working on Serial Communication

--- src/de/bytephil/java/webserver/WebService.java
+++ src/de/bytephil/java/webserver/WebService.java
@@ -1,6 +1,7 @@
 package webserver;
 
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -11,5 +12,15 @@ public class WebService {
 
     public static void boot() {
 
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/public", Location.CLASSPATH);
+            config.showJavalinBanner = false;
+        }).start();
+
+        app.ws("/mainws", ws -> {
+            ws.onMessage(ctx -> {
+                String message = ctx.message().replace("?", "");
+            });
+        });
     }
 }
